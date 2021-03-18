cbc3b5d2bcc46f1e81503a6034b61b4a02e89423
Hugh Burgess <hughburgessgermany@hotmail.com>
2021-03-18 19:12:39 +0100

    4 done

--- 4-from-data/solution.js
+++ 4-from-data/solution.js
@@ -1,3 +1,6 @@
+let button = document.querySelector(".button-add");
+let container = document.querySelector(".container");
+
 const elementsToRender = [
   {
     className: "box",
@@ -16,3 +19,12 @@ const elementsToRender = [
     backgroundColor: "lightseagreen",
   },
 ];
+
+button.addEventListener("click", () => {
+  for (let i = 0; i < 4; i++) {
+    const item = document.createElement("div");
+    item.classList.add(elementsToRender[i].className);
+    item.style.backgroundColor = elementsToRender[i].backgroundColor;
+    container.append(item);
+  }
+});
